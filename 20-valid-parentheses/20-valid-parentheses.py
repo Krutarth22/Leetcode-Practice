31f1f791e358432e3f1ed5f5b846a7a827b047f8
Krutarth22 <krutarthmajithia22@gmail.com>
2022-01-22 15:34:41 -0500

    Time: 38 ms (16.24%), Space: 13.6 MB (34.56%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -7,15 +7,14 @@ class Solution(object):
         d = {'(':')', '{':'}','[':']'}
         
         stack = []
+        
         for i in s:
             
             if i in d:
-                
                 stack.append(i)
                 
-                
             elif len(stack) == 0 or d[stack.pop()] !=i:
-                
                 return False
+            
             
         return len(stack) == 0
