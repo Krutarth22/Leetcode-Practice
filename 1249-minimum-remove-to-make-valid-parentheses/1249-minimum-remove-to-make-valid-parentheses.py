e2a438857e0a23dbf96127ea18fe79029004351d
Krutarth22 <krutarthmajithia22@gmail.com>
2022-01-22 15:31:47 -0500

    Time: 164 ms (57.88%), Space: 15.5 MB (90.65%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -5,28 +5,23 @@ class Solution(object):
         :rtype: str
         """
         s = list(s)
-        
         stack = []
         
         for i, char in enumerate(s):
             
             if char == '(':
-                
                 stack.append(i)
                 
             elif char == ')':
                 
                 if stack:
                     stack.pop()
-                
+                    
+                    
                 else:
-                
                     s[i]=""
                 
-        
         while stack:
-            
-            s[stack.pop()] = ""
-            
+            s[stack.pop()]=""
             
         return "".join(s)
